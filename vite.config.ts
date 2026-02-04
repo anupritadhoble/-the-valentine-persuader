8a91a20b0a4b64d7ae0a3aa188567862d9567437
anupritadhoble <anupritadhoble@gmail.com>
2026-02-04 13:20:46 +0530

    final

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      base: "/the-valentine-persuader/",
+      base: "/-the-valentine-persuader/",
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
